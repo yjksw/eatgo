20f699afaf0ae574e748228a5e6bb7d52801aefc
yunjung <cairofaith@gmail.com>
2020-11-10 14:28:47 +0900

    Restaurant-Detail added

--- eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
@@ -1,22 +1,29 @@
 package com.example.eatgo.interfaces;
 
 import com.example.eatgo.domain.Restaurant;
+import com.example.eatgo.domain.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L, "Bap zip", "Seoul");
-
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = restaurantRepository.findAll();
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = restaurantRepository.findById(id);
+        return restaurant;
     }
 }
--- eatgo-api/src/test/java/com/example/eatgo/interfaces/RestaurantControllerTests.java
+++ eatgo-api/src/test/java/com/example/eatgo/interfaces/RestaurantControllerTests.java
@@ -1,9 +1,12 @@
 package com.example.eatgo.interfaces;
 
+import com.example.eatgo.domain.RestaurantRepository;
+import com.example.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +23,9 @@ class RestaurantControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepository restaurantRepository;
+
     @Test
     public void list() throws Exception {
         mvc.perform(get("/restaurants"))
@@ -29,6 +35,27 @@ class RestaurantControllerTests {
                 ))
                 .andExpect(content().string(
                         containsString("\"name\":\"Bap zip\"")
+                ));
+    }
+
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Bap zip\"")
+                ));
+
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":2020")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Cyber food\"")
                 ));
     }
 }
