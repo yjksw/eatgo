52ad6973087fb2fbceb97f780e9df439970f5a81
yunjung <cairofaith@gmail.com>
2020-11-14 19:58:14 +0900

    Apply Mock Object

--- eatgo-api/src/test/java/com/example/eatgo/interfaces/RestaurantControllerTests.java
+++ eatgo-api/src/test/java/com/example/eatgo/interfaces/RestaurantControllerTests.java
@@ -1,19 +1,21 @@
 package com.example.eatgo.interfaces;
 
 import com.example.eatgo.application.RestaurantService;
-import com.example.eatgo.domain.MenuItemRepository;
-import com.example.eatgo.domain.MenuItemRepositoryImpl;
-import com.example.eatgo.domain.RestaurantRepository;
-import com.example.eatgo.domain.RestaurantRepositoryImpl;
+import com.example.eatgo.domain.*;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,17 +28,18 @@ class RestaurantControllerTests {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
 
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
+
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bap zip", "Seoul"));
+
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
@@ -49,6 +52,14 @@ class RestaurantControllerTests {
 
     @Test
     public void detail() throws Exception {
+
+        Restaurant restaurant1 = new Restaurant(1004L, "Bap zip", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber food", "Seoul");
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
